5c8e41abfb77319239748fd1b682b87d8c7b9e20
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2025-10-19 14:04:27 +0300

    fix: исправлен путь к файлу

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const btnResume = document.querySelector(".info__resume");
 btnResume.addEventListener("click", () => {
     const a = document.createElement("a");
-    a.href = "../assets/resume.pdf";
+    a.href = "./assets/resume.pdf";
     a.download = "resume.pdf";
     document.body.appendChild(a);
     a.click();
